ad1d4901551b5e41df5fc036bd61ffd53076c887
Alex Carl <alexjcarl97@gmail.com>
2024-03-12 20:12:37 -0400

    added csrf token access for development

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', (req, res) => {
-    //setting a cookie on the response with the name of XSRF-TOKEN
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('HELLO WORLD!!')
+// router.get('/hello/world', (req, res) => {
+//     //setting a cookie on the response with the name of XSRF-TOKEN
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('HELLO WORLD!!')
+// });
+
+router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie('XSRF-TOKEN', csrfToken);
+    res.status(200).json({
+        'XSRF-TOKEN' : csrfToken
+    });
 });
+
 
 
 module.exports = router;
