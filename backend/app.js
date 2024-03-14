021f6780efc16d51e225cd5b020c0f970c4e8304
Alex Carl <alexjcarl97@gmail.com>
2024-03-13 21:44:17 -0400

    Add User signup backend endpoint

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     if(err instanceof ValidationError) {
         let errors = {};
-        for(let error of err.erros) {
+        for(let error of err.errors) {
             errors[error.path] = error.message
         }
         err.title = 'Validation error';
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    const { firstName, lastName, userName, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+        firstName,
+        lastName,
+        userName,
+        email,
+        hashedPassword
+    });
+
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.userName
+    };
+
+    await setTokenCookie(res, safeUser)
+
+    return res.json({
+        user: safeUser
+    })
+
+})
 
 module.exports = router;
-
