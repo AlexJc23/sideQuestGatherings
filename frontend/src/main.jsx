b40cd759e3df8dcfc809c4d695ccfa6e7ef78f58
Alex Carl <alexjcarl97@gmail.com>
2024-05-30 21:56:27 -0400

    add csrf token for frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,13 @@ import './index.css';
 
 import { Provider } from 'react';
 import configureStore from './store/store';
-
+import { restoreCSRF, csrfFetch } from './store/csrf';
 const store = configureStore();
 
 if(process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
