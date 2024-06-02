43672a22628709da1518325bd7f07facabf859ce
Alex Carl <alexjcarl97@gmail.com>
2024-06-01 21:21:25 -0400

    bonus modals

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,49 +1,25 @@
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-// import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import LoginFormModal from '../LoginFormModal/LoginFormModal';
-import SignupFormModal from '../SignupFormModal/SignupFormModal';
-
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
 
-  const sessionLinks = sessionUser ? (
-    <>
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    </>
-  ) : (
-    <>
-      <li>
-        <OpenModalButton
-            buttonText='Log In'
-            modalComponent={<LoginFormModal />}
-        />
-      </li>
-      <li>
-        <OpenModalButton
-        buttonText={'Sign Up'}
-        modalComponent={<SignupFormModal />}
-        />
-      </li>
-    </>
-  );
+
 
   return (
-    // <nav className='nav-bar'>
-    <ul className='yeet'>
-      <li>
+    <ul className='nav-bar' >
+      <li >
         <NavLink to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded &&
+      (<li>
+        <ProfileButton user={sessionUser} />
+      </li>)
+    }
     </ul>
-    // </nav>
   );
 }
 
