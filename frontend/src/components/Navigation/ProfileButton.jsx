767458181778f483cac9ca8bda1e102381e1fddb
Alex Carl <alexjcarl97@gmail.com>
2024-06-01 23:06:39 -0400

    .

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -34,7 +34,7 @@ function ProfileButton({ user }) {
 
   const logout = (e) => {
     e.preventDefault();
-    dispatch(sessionActions.logout());
+    dispatch(sessionActions.logoutUser());
     closeMenu();
   };
 
