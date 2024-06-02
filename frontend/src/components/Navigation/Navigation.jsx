a4fb7bf37ebc7f4aecc8a02a8e65dcab163ccccc
Alex Carl <alexjcarl97@gmail.com>
2024-06-02 14:00:45 -0400

    user functionality

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,6 +2,9 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import OpenModalMenuItem from './OpenModalMenuItem';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
@@ -14,10 +17,21 @@ function Navigation({ isLoaded }) {
       <li >
         <NavLink to="/">Home</NavLink>
       </li>
-      {isLoaded &&
+      {isLoaded && sessionUser ?
       (<li>
         <ProfileButton user={sessionUser} />
-      </li>)
+      </li>) : (
+        <div >
+          <OpenModalMenuItem
+          itemText={"Log In"}
+          modalComponent={<LoginFormModal />}
+          />
+          <OpenModalMenuItem
+          itemText={'Sign Up'}
+          modalComponent={<SignupFormModal/>}
+          />
+        </ div>
+      )
     }
     </ul>
   )
