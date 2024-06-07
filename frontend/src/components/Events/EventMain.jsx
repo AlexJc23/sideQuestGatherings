be3fb9cb30eee6e40fe7acd35862aea9b12efddc
Alex Carl <alexjcarl97@gmail.com>
2024-06-07 00:00:52 -0400

    run build

--- frontend/src/components/Events/EventMain.jsx
+++ frontend/src/components/Events/EventMain.jsx
@@ -1,5 +1,5 @@
 import {NavLink} from 'react-router-dom'
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getEvents } from '../../store/events';
 import EventCards from './EventCards';
